0f982b818d452c102726a736ffdb12ed09f6a5f7
07daisuke03 <daisukepapiko@gmail.com>
2021-09-02 22:50:15 +0900

    Header,Footer追加(components作成)

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,21 @@
 <template>
   <div id="app">
     <h1>TodoApp</h1>
+    <app-header></app-header>
+    <app-footer></app-footer>
   </div>
 </template>
 
 <script>
+import Header from './components/Header.vue'
+import Footer from './components/Footer.vue'
 
 export default {
   name: 'App',
+  components: {
+    AppHeader: Header,
+    AppFooter: Footer,
+  }
 }
 </script>
 
