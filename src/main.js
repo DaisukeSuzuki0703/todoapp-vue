a95f673c24b14ca46785977d37fe88577d089baa
07daisuke03 <daisukepapiko@gmail.com>
2021-09-02 21:35:40 +0900

    不要なコンポーネントを削除、vuex,vue-routerの使用環境構築

--- src/main.js
+++ src/main.js
@@ -1,8 +1,19 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueRouter from 'vue-router'
+import store from './store/index.js'
+import routes from './router'
 
 Vue.config.productionTip = false
 
+Vue.use(VueRouter);
+const router = new VueRouter({
+  routes,
+  mode: 'history'
+});
+
 new Vue({
+  store,
+  router,
   render: h => h(App),
 }).$mount('#app')
