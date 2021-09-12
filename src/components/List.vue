2680ba84b9d2006020667e101b5caf80e3976a45
07daisuke03 <daisukepapiko@gmail.com>
2021-09-12 16:55:22 +0900

    リスト表示のマークアップ

--- src/components/List.vue
+++ src/components/List.vue
@@ -1,15 +1,36 @@
 <template>
   <ul class="todo__list">
-    <app-list-item></app-list-item>
+    <app-list-item
+      v-for="todo in todos"
+      :key="todo.id"
+      :todo="todo"
+    ></app-list-item>
   </ul>
 </template>
 
 <script>
-import ListItem from '../components/ListItem'
+import ListItem from "../components/ListItem";
 
-export default ({
+export default {
   components: {
-    AppListItem: ListItem
-  }
-})
+    AppListItem: ListItem,
+  },
+  props: {
+    todos: {
+      type: Array,
+      default: function () {
+        return [];
+      },
+    },
+  },
+};
 </script>
+
+<style scoped>
+.todo__list {
+  text-decoration: none;
+  list-style: none;
+  padding: 0;
+  margin: 0;
+}
+</style>
--- src/components/ListItem.vue
+++ src/components/ListItem.vue
@@ -1,11 +1,43 @@
 <template>
-  <li>
-
+  <li class="todo__item">
+    <div>
+      <p>Title: {{todo.title}}</p>
+      <p>Detail: {{todo.detail}}</p>
+    </div>
+    <div>
+      <button>Edit</button>
+      <button>Delete</button>
+    </div>
   </li>
 </template>
 
 <script>
 
 export default ({
+  props: {
+    id: {
+      type: Number,
+    },
+    todo: {
+      type: Object,
+      default: function() {
+        return {};
+      }
+    }
+  },
 })
 </script>
+
+<style scoped>
+p {
+  padding: 0;
+  margin: 0;
+}
+.todo__item {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 10px;
+  background-color: #f5f5f5;
+}
+</style>
