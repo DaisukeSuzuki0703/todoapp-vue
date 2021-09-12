9eebf292c7fababf9166afae713bf6dfe702a3d2
07daisuke03 <daisukepapiko@gmail.com>
2021-09-12 16:54:53 +0900

    LocalStorageへのTodo登録ロジック実装

--- src/components/Resister.vue
+++ src/components/Resister.vue
@@ -1,55 +1,68 @@
 <template>
   <div class="todo__box">
-    <label for="inputText">Title</label>
-    <input
-      type="text"
-      id="inputText"
-      class="input__area"
-      placeholder="input your Todo title"
-      v-model="targetTodoText"
-    />
-    <label for="inputDetail">Detail</label>
-    <input
-      type="text"
-      id="inputDetail"
-      class="input__area"
-      placeholder="input your Todo detail"
-      v-model="targetTodoDetail"
-    />
+    <div>
+      <label for="inputText">Title</label>
+      <input
+        type="text"
+        id="inputText"
+        class="input__area"
+        placeholder="input your Todo title"
+        v-model="targetTodoText"
+      />
+      <label for="inputDetail">Detail</label>
+      <input
+        type="text"
+        id="inputDetail"
+        class="input__area"
+        placeholder="input your Todo detail"
+        v-model="targetTodoDetail"
+      />
+    </div>
+    <div class="submit">
+      <button class="submitbtn" @click="submit">送信</button>
+    </div>
   </div>
 </template>
 
 <script>
-export default ({
+export default {
   computed: {
     targetTodoText: {
       get() {
-        return this.$store.state.targetTodo.title
+        return this.$store.state.targetTodo.title;
       },
       set(value) {
-        return this.$store.dispatch('updateTextValue', {
-          name: 'title',
-          value: value
-        })
-      }
+        return this.$store.dispatch("updateTextValue", {
+          name: "title",
+          value: value,
+        });
+      },
     },
     targetTodoDetail: {
       get() {
-        return this.$store.state.targetTodo.detail
+        return this.$store.state.targetTodo.detail;
       },
       set(value) {
-        return this.$store.dispatch('updateTextValue', {
-          name: 'detail',
-          value: value
-        })
-      }
+        return this.$store.dispatch("updateTextValue", {
+          name: "detail",
+          value: value,
+        });
+      },
+    },
+  },
+  methods: {
+    submit() {
+      this.$store.dispatch('resisterTodo');
     }
   }
-})
+};
 </script>
 
 
 <style scoped>
+.todo__box {
+  margin-bottom: 20px;
+}
 .todo__box > label {
   font-size: 16px;
 }
@@ -60,5 +73,9 @@ export default ({
 }
 .input__area:last-child {
   margin-bottom: 0;
+}
+.submit {
+  text-align: right;
+  margin-top: 10px;
 }
 </style>
--- src/store/index.js
+++ src/store/index.js
@@ -6,22 +6,39 @@ Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
+    todoKeyWord: 'Todos',
     todos: [],
     targetTodo: {
-      id: '',
+      id: 1,
       title: '',
       detail: '',
+      completed: false,
     },
 
   },
   mutations: {
     updateTextValue(state, payload) {
       state.targetTodo[payload.name] = payload.value;
+    },
+    resisterTodo(state, targetTodo) {
+      state.todos.push(targetTodo);
+      localStorage.setItem(state.todoKeyWord, JSON.stringify(state.todos));
     }
   },
   actions: {
     updateTextValue({commit}, {value, name}) {
-    commit('updateTextValue', {value, name});
+      commit('updateTextValue', {value, name});
+    },
+    resisterTodo({commit, state}) {
+      const targetTodo = Object.assign({}, {
+        title: state.targetTodo.title,
+        detail: state.targetTodo.detail,
+        id: state.targetTodo.id++,
+        completed: false,
+      })
+      commit('resisterTodo', targetTodo);
+      state.targetTodo.title = '';
+      state.targetTodo.detail = '';
     }
   }
 });
