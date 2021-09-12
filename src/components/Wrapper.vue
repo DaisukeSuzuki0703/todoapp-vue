556774f97f543a893d7db1d61bb7d13c1e6f389e
07daisuke03 <daisukepapiko@gmail.com>
2021-09-12 18:16:05 +0900

    header, footerコメントアウト

--- src/components/Wrapper.vue
+++ src/components/Wrapper.vue
@@ -1,24 +1,24 @@
 <template>
   <div class="wrapper">
-    <app-header></app-header>
+    <!-- <app-header></app-header> -->
     <main class="main">
         <slot />
         <div class="todos">
           <slot name="todos" />
         </div>
       </main>
-    <app-footer></app-footer>
+    <!-- <app-footer></app-footer> -->
   </div>
 </template>
 
 <script>
-import Header from '../components/Header'
-import Footer from '../components/Footer'
+// import Header from '../components/Header'
+// import Footer from '../components/Footer'
 
 export default ({
   components: {
-    AppHeader: Header,
-    AppFooter: Footer,
+    // AppHeader: Header,
+    // AppFooter: Footer,
   }
 })
 </script>
